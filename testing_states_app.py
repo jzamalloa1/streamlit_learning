a6df49c2085425cc821b4a7517e1dd7636610bbc
JoseZ <jzamalloa1@gmail.com>
2024-02-11 23:43:01 -0500

    Done with learning basics and states. Ready for LLM app

--- testing_states_app.py
+++ testing_states_app.py
@@ -18,6 +18,19 @@ st.markdown("""So the session will be run everytime the user interacts with the 
          The session state will only be reset when the page is refreshed, a new tab is open, and
          will be different for different users.""")
 
+st.divider()
+
+# Create data and tie to the session state so it doesn't refresh within the same session
+st.markdown("""We are tying the randomized created data as a key to the session state
+            This way, the data will not be re-initialized everytime, even when the page is ran,
+            as long as are within the same session state
+            """)
+if "chart_data" not in st.session_state:
+    st.session_state.chart_data = pd.DataFrame(
+        np.random.randn(20, 3),
+        columns=['a', 'b', 'c']
+        )
+
 # Add Title
 st.header("Testing States in Streamlit", divider="rainbow")
 
@@ -26,13 +39,7 @@ lc, rc = st.columns(2, gap="medium")
 
 with lc:
     st.subheader("Left column")
-
-    chart_data = pd.DataFrame(
-        np.random.randn(20, 3),
-        columns=['a', 'b', 'c']
-        )
-
-    st.line_chart(data=chart_data)
+    st.line_chart(data=st.session_state.chart_data)
 
 with rc:
     st.subheader("Right column")

b2739c9348f0970ee6e6362078882d76bac7301a
JoseZ <jzamalloa1@gmail.com>
2024-02-11 23:25:01 -0500

    Evaluating session state on refresh, tabs and counters

--- testing_states_app.py
+++ testing_states_app.py
@@ -2,6 +2,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# Check the session state
+if "counter" not in st.session_state: #If not in session state, initialize it. 
+    # Theoretically this will be initialized when refresh, will be different for different users, and different in different tabs
+    st.session_state.counter = 0
+
+st.session_state.counter += 1 # Add one every time we run it again
+
+st.header(f"This page has run {st.session_state.counter} times.")
+st.button("Run it again")   
+
+st.markdown("""So the session will be run everytime the user interacts with the page (script).
+         If the user clicks a button (selectbox, radio, etc), then the counter will increase
+         (page runs once more) since the 'counter' key already exists in the session state (initialized).
+         The session state will only be reset when the page is refreshed, a new tab is open, and
+         will be different for different users.""")
+
 # Add Title
 st.header("Testing States in Streamlit", divider="rainbow")
 

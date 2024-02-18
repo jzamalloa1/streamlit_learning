22a4211052c55b53577b46d76e93d09a1ea753c5
JoseZ <jzamalloa1@gmail.com>
2024-02-18 00:06:31 -0500

    Finished first version of GPT working App, with ChatOpenAI, memory and stream capabilities

---
+++ chat_gpt_bot.py
@@ -0,0 +1,67 @@
+# WE'LL BUILD A FULL CHAT-GPT APP
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+
+st.title("Gordolize AI")
+
+# Set API key
+os.environ["OPENAI_API_KEY"] = ""
+
+# Initiatize in session state default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+# Initialize [rendering] chat history to contain messages
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+# Initalize LLM's chat history
+if "ai_messages" not in st.session_state:
+    st.session_state["ai_messages"] = []
+
+# Initialize LLM model
+llm = ChatOpenAI(model=st.session_state["openai_model"], 
+                 temperature=0.1, streaming=True)
+
+# Render chat messages in role containers
+for m in st.session_state["messages"]:
+    with st.chat_message(m["role"]):
+        st.markdown(m["content"])
+
+# Accept and render initial user input
+prompt = st.chat_input("Como quieres que El Gordo te ilumine hoy?")
+
+if prompt:
+
+    # Render to user's container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    # Store in message and ai_message history session state
+    st.session_state.messages.append({"role":"user", "content":prompt})
+
+    st.session_state.ai_messages.append(
+        HumanMessage(content=prompt)
+        )
+
+    # Get ChatGPT answer, render it and store it
+    with st.chat_message("assistant"):
+        
+        # Run model with message history (ChatOpenAI by default takes in chat request, since after all is a chat model)
+        #   and in streaming mode
+        ai_response = llm.stream(st.session_state.ai_messages)
+
+        # Render streaming response to AI's container AND store in object to be able to add it to chat history and AI's chat history
+        ai_response_content = st.write_stream(ai_response)
+
+        # Store in message and ai_message history session state
+        st.session_state.messages.append({"role":"assistant", "content": ai_response_content})
+
+        st.session_state.ai_messages.append(
+            AIMessage(content=ai_response_content)
+        )
